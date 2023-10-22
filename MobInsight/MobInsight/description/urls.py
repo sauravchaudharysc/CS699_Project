4a758e7a768887805d637372b83a22a1061159a7
vedantgoswami <vedant.goswami6227@gmail.com>
2023-10-22 13:45:59 +0530

    added graph

--- MobInsight/MobInsight/description/urls.py
+++ MobInsight/MobInsight/description/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("",views.HomeView.as_view(),name="homepage"),
+    path("stats",views.StatView.as_view(),name="statpage"),
     path("get-names/",views.get_names),
     path("<str:item>/",views.item_single,name="item_single")
 ]
--- MobInsight/MobInsight/description/views.py
+++ MobInsight/MobInsight/description/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import get_object_or_404,render
 from django.views.generic import ListView
 from django.http import JsonResponse
-
+import numpy as np
+import matplotlib.pyplot as plt
 from .models import Item
-
+from io import BytesIO
+import base64
 # Create your views here.
 class HomeView(ListView):
     model = Item
@@ -15,10 +17,46 @@ class HomeView(ListView):
             return 'description/components/item-list-elements.html'
         return "description/index.html"
 
+class StatView(ListView):
+    model = Item
+    def get_template_names(self):
+        return "description/stats.html"
+
+def get_graph():
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    image_png = buffer.getvalue()
+    graph = base64.b64encode(image_png)
+    graph = graph.decode('utf-8')
+    buffer.close()
+    return graph
+
+def get_plot():
+    fb = (120, 90, 110, 100, 95)
+    yt = (80, 90, 75, 85, 95)
+    ig = (50, 40, 55, 35, 45)
+    x = ['Performance', 'Display', 'Camera', 'Battery', 'Storage']
+    xpos = np.arange(len(x))
+    barWidth = 0.4
+    plt.switch_backend('AGG')
+    plt.figure(figsize=(7,5))
+    plt.bar(xpos, fb, color='royalblue', width= barWidth, label='FB')
+    plt.bar(xpos, yt, bottom=fb, color='red', width= barWidth, label='YT')
+    plt.bar(xpos, ig, bottom=yt, color='purple', width= barWidth, label='IG')
+    plt.xlabel('Specification')
+    plt.ylabel('Rating')
+    plt.title('Comparison')
+    plt.xticks(xpos, x)
+    plt.legend()
+    graph = get_graph()
+    return graph
+
+
 def item_single(request, item):
     item = get_object_or_404(Item, name=item)
-    return render(request, "description/single.html",{"item":item})
-
+    chart = get_plot()
+    return render(request, "description/single.html",{"item":item, "chart": chart})
 
 def get_names(request):
     search = request.GET.get('search')
